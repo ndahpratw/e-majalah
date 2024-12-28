f1f7833dbbaef6de4a8166478eeace496f0bd19d
ndhpratw <bolabolabangkalan5@gmail.com>
2024-12-28 13:40:39 +0700

    frontend home

---
+++ resources/views/pages/user/index.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <title>Home</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+      
+        <!-- Favicons -->
+        <link href="{{ asset('assets/img/logo.png') }}" rel="icon">
+        <link href="{{ asset('assets/img/logo.png') }}" rel="logo">
+      
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+      
+        <!-- Vendor CSS Files -->
+        <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+
+        <!-- Template Main CSS File -->
+        <link href="{{ asset('assets/css/style-user.css') }}" rel="stylesheet">
+      
+        <!-- =======================================================
+        * Template Name: Arsha
+        * Updated: Sep 18 2023 with Bootstrap v5.3.2
+        * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+        * Author: BootstrapMade.com
+        * License: https://bootstrapmade.com/license/
+        ======================================================== -->
+      </head>
+<body>
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center" style="background-image: url({{ asset('assets/img/index.jpeg')}}); background-size: cover; background-position: center; height:100vh;" alt="">
+    <div class="container">
+        <div class="row">
+            <div class="d-flex justify-content-center align-items-center">
+                <a href="" class="btn-get-started scrollto">Jelajahi</a>
+              </div>
+        </div>
+    </div>
+    </section><!-- End Hero -->
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('pages/user/index');
 });
 
 Route::get('/dashboard', function () {
