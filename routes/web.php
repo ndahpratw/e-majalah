6545d809baaa11985e8031517362b2fd5d45436d
ndhpratw <bolabolabangkalan5@gmail.com>
2024-12-31 00:46:23 +0700

    update frontend cover

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages/user/index');
+    return view('pages/user/main');
 });
 
 Route::get('/dashboard', function () {
