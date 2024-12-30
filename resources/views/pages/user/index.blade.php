70725a2c320eee33a277873ae80e840cdcede658
ndhpratw <bolabolabangkalan5@gmail.com>
2024-12-30 12:06:57 +0700

    update frontend cover

--- resources/views/pages/user/index.blade.php
+++ resources/views/pages/user/index.blade.php
@@ -12,7 +12,10 @@
         <link href="{{ asset('assets/img/logo.png') }}" rel="logo">
       
         <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">        
+        {{-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> --}}
       
         <!-- Vendor CSS Files -->
         <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
@@ -41,6 +44,32 @@
                 padding: 10px 20px;
                 font-size: 1.25rem;
             }
+
+            .wawasan {
+                font-size: 10rem !important;
+                line-height: 1.2;
+            }
+
+            .pacifico-regular {
+                font-family: "Pacifico", serif;
+                font-weight: 400;
+                font-style: normal;
+                /* color: white; Warna isi teks */
+                text-shadow: 
+                    2px 2px 0px blue,  /* Bayangan di kanan bawah */
+                    -2px 2px 0px blue, /* Bayangan di kiri bawah */
+                    2px -2px 0px blue, /* Bayangan di kanan atas */
+                    -2px -2px 0px blue, /* Bayangan di kiri atas */
+                    0px 2px 0px blue,  /* Bayangan di bawah */
+                    2px 0px 0px blue,  /* Bayangan di kanan */
+                    -2px 0px 0px blue, /* Bayangan di kiri */
+                    0px -2px 0px blue; /* Bayangan di atas */
+            }
+
+
+            .rock {
+                height: 175px;;
+            }
         
             /* Styling khusus untuk mode ponsel */
             @media (max-width: 576px) {
@@ -52,14 +81,53 @@
                 .center-button {
                     top: 60%; /* Atur tombol sedikit lebih ke bawah untuk proporsi yang baik */
                 }
+
+                .rock {
+                    height: 225px;
+                }
+
+                .wawasan {
+                    font-size: 5rem !important;
+                    line-height: 1.2;
+                }
             }
         </style>
         
     </head>
     <body>
         <!-- ======= Hero Section ======= -->
-        {{-- <section id="hero" class="d-flex align-items-center" style="background-image: url({{ asset('assets/img/index.jpeg')}}); background-size: cover; background-position: center; height:100vh;" alt=""> --}}
-        <section id="hero" class="d-flex align-items-center" style="background-color:black; height:100vh;" alt="">
+
+        {{-- satu --}}
+        <section id="hero" style="background-image: url({{ asset('assets/img/index.jpeg') }}); background-size: cover; background-position: center; height: 100vh; position: relative; margin: 0;">
+            <div class="container-fluid" style="height: 100%; padding: 0;">
+                <!-- Row untuk pojok kiri atas -->
+                <div class="row" style="position: absolute; top: 0; left: 0; width: 100%; margin: 0; display: flex; justify-content: space-between;">
+                    <!-- Kolom kiri -->
+                    <div class="col-5 bg-dark rock"></div>
+                    <!-- Kolom kanan -->
+                    <div class="col-1 bg-dark" style="height: 225px"></div>
+                </div>
+
+                <!-- Elemen di tengah -->
+                <div class="row" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); margin: 0; text-align: center; display: flex; justify-content: center; align-items: center; white-space: nowrap;">
+                    <h1 class="wawasan text-white fw-bold mb-0">Wawasan</h1>
+                    <h1 class="pacifico-regular text-white d-inline ms-2 mb-0">Tak Terbatas</h1>
+                    <a href="#" class="btn btn-secondary rounded-circle btn-lg ms-3 d-flex justify-content-center align-items-center" style="width: 60px; height: 60px;">
+                        <i class="bi bi-play-fill"></i>
+                    </a>
+                </div>
+                
+                <!-- Row untuk pojok kiri bawah -->
+                <div class="row" style="position: absolute; bottom: 0; left: 0; width: 100%; margin: 0; display: flex; justify-content: space-between;">
+                    <!-- Kolom kiri -->
+                    <div class="col-5 bg-dark rock"></div>
+                </div>
+        
+            </div>
+        </section>
+        
+        {{-- dua --}}
+        {{-- <section id="hero" class="d-flex align-items-center" style="background-color:black; height:100vh;" alt="">
             <div class="container position-relative">
                 <!-- Judul -->
                 <h1 class="text-center text-white position-absolute w-100 overlap-title">
@@ -86,6 +154,6 @@
                 <!-- Tombol di tengah -->
                 <a href="" class="btn btn-secondary rounded-circle btn-lg position-absolute center-button"><i class="bi bi-play-fill"></i></a>
             </div>            
-        </section><!-- End Hero -->
+        </section><!-- End Hero --> --}}
     </body>
 </html>
